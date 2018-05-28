1f2ce566fb46c2128a1ccdc07b83f946d45b8453
Martin Polanka <PolankaMartin@gmail.com>
2018-05-28 22:27:12 +0200

    Empty requests factories for scenario actions

---
+++ src/main/java/cz/polankam/pcrf/trafficgenerator/scenario/actions/factory/GxRequestsFactory.java
@@ -0,0 +1,86 @@
+package cz.polankam.pcrf.trafficgenerator.scenario.actions.factory;
+
+import cz.polankam.pcrf.trafficgenerator.scenario.ScenarioContext;
+import org.jdiameter.api.AvpSet;
+import org.jdiameter.api.gx.ClientGxSession;
+import org.jdiameter.api.gx.events.GxCreditControlRequest;
+import org.jdiameter.common.impl.app.gx.GxCreditControlRequestImpl;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class GxRequestsFactory {
+
+    public static GxCreditControlRequest createCcrI(ScenarioContext context, boolean isFramedIp, boolean isSubscriptionId) throws Exception {
+        ClientGxSession session = context.getGxSession();
+        ConcurrentHashMap<String, Object> state = context.getState();
+        int requestNumber = (int) state.get("cc_request_number");
+        state.put("cc_request_number", requestNumber + 1);
+
+        // *** CREATE REQUEST
+
+        GxCreditControlRequestImpl req = new GxCreditControlRequestImpl(session, context.getGxRealm(), context.getGxServerUri());
+        AvpSet avps = req.getMessage().getAvps();
+
+        // TODO
+
+        // *** RETURN REQUEST
+
+        return req;
+    }
+
+    public static GxCreditControlRequest createCcrT(ScenarioContext context, boolean isSubscriptionId) throws Exception {
+        ClientGxSession session = context.getGxSession();
+        ConcurrentHashMap<String, Object> state = context.getState();
+        int requestNumber = (int) state.get("cc_request_number");
+        state.put("cc_request_number", requestNumber + 1);
+
+        // *** CREATE REQUEST
+
+        GxCreditControlRequestImpl req = new GxCreditControlRequestImpl(session, context.getGxRealm(), context.getGxServerUri());
+        AvpSet avps = req.getMessage().getAvps();
+
+        // TODO
+
+        // *** RETURN REQUEST
+
+        return req;
+    }
+
+    public static GxCreditControlRequest createCcrU(ScenarioContext context, boolean isSubscriptionId) throws Exception {
+        ClientGxSession session = context.getGxSession();
+        ConcurrentHashMap<String, Object> state = context.getState();
+        int requestNumber = (int) state.get("cc_request_number");
+        state.put("cc_request_number", requestNumber + 1);
+
+        // *** CREATE REQUEST
+
+        GxCreditControlRequestImpl req = new GxCreditControlRequestImpl(session, context.getGxRealm(), context.getGxServerUri());
+        AvpSet avps = req.getMessage().getAvps();
+
+        // TODO
+
+        // *** RETURN REQUEST
+
+        return req;
+    }
+
+    public static GxCreditControlRequest createCcrU_LostConnection(ScenarioContext context) throws Exception {
+        ClientGxSession session = context.getGxSession();
+        ConcurrentHashMap<String, Object> state = context.getState();
+        int requestNumber = (int) state.get("cc_request_number");
+        state.put("cc_request_number", requestNumber + 1);
+
+        // *** CREATE REQUEST
+
+        GxCreditControlRequestImpl req = new GxCreditControlRequestImpl(session, context.getGxRealm(), context.getGxServerUri());
+        AvpSet avps = req.getMessage().getAvps();
+
+        // TODO
+
+        // *** RETURN REQUEST
+
+        return req;
+    }
+
+}
---
+++ src/main/java/cz/polankam/pcrf/trafficgenerator/scenario/actions/factory/RxRequestsFactory.java
@@ -0,0 +1,48 @@
+package cz.polankam.pcrf.trafficgenerator.scenario.actions.factory;
+
+import cz.polankam.pcrf.trafficgenerator.scenario.ScenarioContext;
+import org.jdiameter.api.AvpSet;
+import org.jdiameter.api.rx.ClientRxSession;
+import org.jdiameter.api.rx.events.RxAARequest;
+import org.jdiameter.api.rx.events.RxSessionTermRequest;
+import org.jdiameter.common.impl.app.rx.RxAARequestImpl;
+import org.jdiameter.common.impl.app.rx.RxSessionTermRequestImpl;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class RxRequestsFactory {
+
+    public static RxAARequest createAar(ScenarioContext context, int bandwidth, String[] codecData, String[] firstFlowDesc, String[] secondFlowDesc) throws Exception {
+        ClientRxSession session = context.getRxSession();
+        ConcurrentHashMap<String, Object> state = context.getState();
+
+        // *** CREATE REQUEST
+
+        RxAARequestImpl req = new RxAARequestImpl(session, context.getRxRealm(), context.getRxServerUri());
+        AvpSet avps = req.getMessage().getAvps();
+
+        // TODO
+
+        // *** RETURN REQUEST
+
+        return req;
+    }
+
+    public static RxSessionTermRequest createStr(ScenarioContext context) throws Exception {
+        ClientRxSession session = context.getRxSession();
+        ConcurrentHashMap<String, Object> state = context.getState();
+
+        // *** CREATE REQUEST
+
+        RxSessionTermRequestImpl req = new RxSessionTermRequestImpl(session, context.getRxRealm(), context.getRxServerUri());
+        AvpSet avps = req.getMessage().getAvps();
+
+        // TODO
+
+        // *** RETURN REQUEST
+
+        return req;
+    }
+
+}
