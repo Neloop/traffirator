d07b2830e18e68f8fbb4bca7b1e80c0f9069aafa
Martin Polanka <PolankaMartin@gmail.com>
2018-06-13 14:43:56 +0200

    New call performance configuration

--- examples/generate-performance.py
+++ examples/generate-performance.py
@@ -1,6 +1,6 @@
 for i in range(0, 144, 2):
-    count = i / 2 * 100
+    count = i / 2 * 300
     print "  - start: {}".format(i * 60 * 1000)
     print "    scenarios:"
     print "      - type: CallPerformance"
-    print "        count: {}".format(count + 100)
+    print "        count: {}".format(count + 300)
